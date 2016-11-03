831a147182ad92ed0f8a6a081a1f7a0f6d19b481
kakadu <kakadu.hafanana@gmail.com>
2016-11-03 17:14:01 +0300

    more tests

--- regression/performance/test002.mu.scm
+++ regression/performance/test002.mu.scm
@@ -1,6 +1,7 @@
 (include "../../microKanren/microKanren.scm")
 (include "../../microKanren/miniKanren-wrappers2.scm")
 
+
 (define poso
   (lambda (n)
     (fresh (a d)
@@ -15,7 +16,11 @@
        (lambda ()
          ((fives x) a/c))))))
 
-; without display REPL prints the result but compiled code doesn't
 (display
  (run* (q)
-   (expo '(1 1) '(1 0 1) q)))
+   (logo '(0 1 1 1) '(0 1) '(1 1) q)))
+
+; without display REPL prints the result but compiled code doesn't
+;(display
+;  (run* (q)
+;    (expo '(1 1) '(1 0 1) q)))
