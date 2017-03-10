d45dcbacaa255d312c0170056c956f4277373bb8
kakadu <kakadu.hafanana@gmail.com>
2017-03-10 17:26:53 +0300

    WIP

--- regression/test_camlp5.ml
+++ regression/test_camlp5.ml
@@ -1,9 +1,12 @@
-@type token_env = int with show,gmap
+(*
+  camlp5o -I `ocamlfind query GT` pa_gt.cmo show.cmo gmap.cmo pr_o.cmo regression/test_camlp5.ml
+*)
+type token_env = int
 
-type 'a logic =
-| Var   of GT.int * 'a logic GT.list
+@type 'a logic =
+| Var   of GT.int * 'a logic GT.list * 'a logic
 | Value of 'a
-
+with show,gmap
 
 
 type ('a, 'l) llist = Nil | Cons of 'a * 'l
