8df3bcc4c5eebb7de6a0aef470e2216ee0bc3799
kakadu <kakadu.hafanana@gmail.com>
2017-03-14 12:53:20 +0300

    Fix generation of signature

--- regression/test100ppxgt.mli
+++ regression/test100ppxgt.mli
@@ -1,9 +1,17 @@
+(*
+  to get preprocessed version by PPX use
+  make ppx && ocamlfind ppx_tools/rewriter "`ocamlfind query ppx_deriving`/ppx_deriving -deriving-plugin `ocamlfind query ppx_deriving`/ppx_deriving_show.cmo -deriving-plugin _build/ppx/ppx_deriving_gt.cma" -intf regression/test100ppxgt.mli
+*)
+
 type token_env = int
 
 type 'a logic =
 | Var   of GT.int * 'a logic GT.list
 | Value of 'a
 [@@deriving gt {show} ]
+
+(* Next line because we override logic *)
+(* val logic : (unit,< show: ('a ->  string) -> 'a logic -> string > ) GT.t *)
 
 (* type ('a, 'l) llist = Nil | Cons of 'a * 'l
 [@@deriving gt {show} ]
