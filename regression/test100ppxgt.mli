32df9d8ef313b051a9ccb39a93b1942b008a3933
kakadu <kakadu.hafanana@gmail.com>
2017-03-14 16:01:28 +0300

    more tests

--- regression/test100ppxgt.mli
+++ regression/test100ppxgt.mli
@@ -3,18 +3,19 @@
   make ppx && ocamlfind ppx_tools/rewriter "`ocamlfind query ppx_deriving`/ppx_deriving -deriving-plugin `ocamlfind query ppx_deriving`/ppx_deriving_show.cmo -deriving-plugin _build/ppx/ppx_deriving_gt.cma" -intf regression/test100ppxgt.mli
 *)
 
-type token_env = int
+(* type token_env = int *)
 
-type 'a logic =
+(* type 'a logic =
 | Var   of GT.int * 'a logic GT.list
 | Value of 'a
-[@@deriving gt {show} ]
+[@@deriving gt {show} ] *)
 
 (* Next line because we override logic *)
 (* val logic : (unit,< show: ('a ->  string) -> 'a logic -> string > ) GT.t *)
 
 (* type ('a, 'l) llist = Nil | Cons of 'a * 'l
 [@@deriving gt {show} ]
+*)
 
-type 'a lnat = O | S of 'a
+(* type 'a lnat = O | S of 'a
  [@@deriving gt { show } ] *)
