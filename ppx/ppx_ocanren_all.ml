89388937cb748751ae382b7e8577b662f862bb9b
kakadu <kakadu.hafanana@gmail.com>
2017-03-15 14:54:03 +0300

    prettify code

--- ppx/ppx_ocanren_all.ml
+++ ppx/ppx_ocanren_all.ml
@@ -5,6 +5,7 @@ open Ast_405
 (* fresh *)
 let () = Driver.register ~name:"pa_minikanren" Versions.ocaml_405 (fun  _ _ -> Smart_logger.pa_minikanren)
 
+(* REPR constructor for testing *)
 let () = Ppx_repr.register ()
 
 let _ = Driver.run_main ()
