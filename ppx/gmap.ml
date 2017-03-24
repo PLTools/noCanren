9953a3264d60967fb08a7ef23fda18b18b46b56d
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2017-03-24 18:30:39 +0300

    gmap plugin implementation

--- ppx/gmap.ml
+++ ppx/gmap.ml
@@ -13,18 +13,8 @@ open Ast_helper
 open Location
 open Ppx_deriving
 
-module Exp =
-  struct
-    include Exp
+module Exp = Show.Exp
 
-    let make_pair a b = tuple [a;b]
-    let make_list xs =
-      List.fold_right (fun e acc -> construct (lid "::") (Some (make_pair e acc)) ) xs
-                                              (Exp.construct (lid "[]") None)
-  end
-
-(* Used when we need to check that type we working on references himself in
-  it's body *)
 let are_the_same (typ: core_type) (tdecl: type_declaration) =
   (* Pprintast.core_type Format.std_formatter (Obj.magic typ);
   Format.pp_force_newline Format.std_formatter ();
@@ -54,14 +44,14 @@ let expr_of_arg reprname typ root_type =
    [%expr [%e Exp.(send [%expr subj.GT.t] (mknoloc _alpha)) ] ]
   | [%type: int]
   | [%type: GT.int] ->
-   maybe_apply [%expr GT.lift GT.int.GT.plugins#show () ]
+   maybe_apply [%expr GT.lift GT.int.GT.plugins#gmap () ]
   | [%type: string]
   | [%type: GT.string] ->
-   maybe_apply [%expr GT.transform GT.string (new GT.show_string_t) () ]
+   maybe_apply [%expr GT.transform GT.string (new GT.gmap_string_t) () ]
   | [%type: [%t? t] GT.list]
   | [%type: [%t? t] list] ->
-   maybe_apply [%expr GT.lift (GT.list.GT.plugins#show [%e helper t]) () ]
-  | {ptyp_desc=Ptyp_constr ({txt=Lident cname;_},
+   maybe_apply [%expr GT.lift (GT.list.GT.plugins#gmap [%e helper t]) () ]
+  (* | {ptyp_desc=Ptyp_constr ({txt=Lident cname;_},
                            [{ptyp_desc=Ptyp_constr({txt=Lident argname;_},
                                                    _)
                             }]); _ }
@@ -82,7 +72,7 @@ let expr_of_arg reprname typ root_type =
                ([%e head] this)
                [%e Exp.(new_ @@ lid @@ sprintf "show_%s_t" cname) ]
                ()
-       ]
+       ] *)
   | {ptyp_desc=Ptyp_constr ({txt=Lident cname;_},
                            [typ_arg1]); }
    ->
@@ -90,7 +80,7 @@ let expr_of_arg reprname typ root_type =
      [%expr  GT.transform
                [%e Exp.ident @@ lid cname]
                [%e helper  typ_arg1 ]
-               [%e Exp.(new_ @@ lid @@ sprintf "show_%s_t" cname) ]
+               [%e Exp.(new_ @@ lid @@ sprintf "gmap_%s_t" cname) ]
                ()
      ]
   | _ ->
@@ -100,23 +90,26 @@ let expr_of_arg reprname typ root_type =
   match typ with
   | {ptyp_desc=Ptyp_var _alpha; _} ->
    [%expr [%e Exp.(field (ident @@ lid reprname) (lid "GT.fx")) ] () ]
-  (* | _ when are_the_same typ root_type -> *)
   | _ -> [%expr [%e helper ~toplevel:true typ ]
-         (* () [%e Exp.ident @@ lid reprname ] *)
          ]
 
-let name = "show"
 
-let extra_params _ = []
+let name = "gmap"
+
+let extra_params root_type = [([%type: 'sa], Invariant)]
 
 let inh _  = [%type: unit]
-let synh _ = [%type: string]
+let synh s = Typ.var @@ "s" ^s
 
-let synh_root _ _ = [%type: string]
+let synh_root root_type params =
+  Typ.constr (lid root_type.ptype_name.txt) params
+
 
 let core = function
   | [%type: int] ->
-    Cl.structure (Cstr.mk (Pat.any ()) [ Cf.inherit_ Fresh (Cl.constr (lid "GT.show_int_t") []) None ])
+     Cl.structure (Cstr.mk (Pat.any ())
+      [ Cf.inherit_ Fresh (Cl.constr (lid "GT.gmap_int_t") []) None
+      ])
   | t ->
     let b = Buffer.create 40 in
     let fmt = Format.formatter_of_buffer b in
@@ -131,18 +124,16 @@ let constructor root_type constr =
     let arg_names = List.mapi (fun n _ -> sprintf "p%d" n) arg_types in
     let body =
       match List.combine arg_names arg_types with
-      | [] -> Exp.constant (Pconst_string (name.txt ^ " ()", None))
-      | [(argn,argt)] -> [%expr
-                            [%e Exp.constant (Pconst_string (name.txt ^ " (", None)) ] ^
-                            [%e expr_of_arg argn argt root_type] ^ ")"
-                         ]
+      | [] -> Exp.construct (lid name.txt) None
+      | [(argn, argt)] -> Exp.construct (lid name.txt) @@ Some (expr_of_arg argn argt root_type)
       | args ->
          let xs = List.map (fun (argn,argt) -> expr_of_arg argn argt root_type) args in
          (* [%expr 1] *)
-         [%expr
+         Exp.construct (lid name.txt) @@ Some (Exp.tuple xs)
+         (* [%expr
              [%e Exp.constant (Pconst_string (name.txt ^ " (", None)) ] ^
              (String.concat ", " [%e Exp.make_list xs ] ^ ")")
-         ]
+         ] *)
     in
     let e = List.fold_right (fun name acc -> Exp.fun_ Nolabel None (Pat.var @@ mknoloc name) acc) ("inh"::"subj"::arg_names) body in
     Cf.method_ (mknoloc @@ "c_" ^ name.txt) Public (Cfk_concrete (Fresh, e))
