7c6740abf8493a4c508c943be52141ed73dcadbd
kakadu <kakadu.hafanana@gmail.com>
2017-05-01 01:33:01 +0300

    It works for simple tests but something is wrong with diseq

--- regression/test001.ml
+++ regression/test001.ml
@@ -29,26 +29,25 @@ let show_intl_list = (show(List.logic ) (show(logic) (show int)))
 let show_logic_list h xs = show_intl_list @@ List.reify ManualReifiers.int_reifier h xs
 
 let rec appendo a b ab =
-  (* trace "appendo" @@ *)
   let (===) = unitrace show_logic_list in
   conde
     [ (*project3 "appendo simple (a,b,ab): " show_logic_list a b ab &&& *)
       ((a === nil ()) &&& (b === ab))
-    ; Fresh.two (fun h t ->
+    ; Fresh.three (fun h t ab' ->
           (* project3 "appendo complex (a,b,ab): " show_logic_list a b ab &&& *)
           (a === h%t) &&&
-          Fresh.one (fun ab' ->
-              (h%ab' === ab) &&& (appendo t b ab')
-          )
+          (h%ab' === ab) &&& (appendo t b ab')
         )
     ]
 
 let runL n         = runR (List.reify ManualReifiers.int_reifier) show_int_list show_intl_list n
 
 let rec reverso a b =
-  (* trace "reverso" @@ *)
+  (* let (===) = unitrace (fun h t -> GT.(show List.logic @@ show logic (show int)) @@
+    List.reify ManualReifiers.int_reifier h t)
+  in *)
   conde
-    [ (fun st -> MKStream.bind ((===) a (nil ()) st) (b === nil ()))
+    [ ((a === nil ()) &&& (b === nil ()))
     ; fresh (h t a')
         (a === h%t)
         (appendo a' !<h b)
@@ -58,10 +57,13 @@ let rec reverso a b =
 
 let _ =
   run_exn show_int_list  1  q qh (REPR (fun q   -> appendo q (ilist [3; 4]) (ilist [1; 2; 3; 4])   ));
-  (* run_exn show_int_list  1  q qh (REPR (fun q   -> reverso q (ilist [1; 2; 3; 4])                  ));
+  (* run_exn show_int_list  1  q qh (REPR (fun q   -> reverso q (ilist [1; 2; 3; 4])                  )); *)
+  (* run_exn show_int_list  1  q qh (REPR (fun q   -> appendo q (ilist []) (ilist [1])   )); *)
+  (* run_exn show_int_list  1  q qh (REPR (fun q   -> appendo q (ilist [3; 4]) (ilist [1; 2; 3; 4])   ));
+  run_exn show_int_list  1  q qh (REPR (fun q   -> reverso q (ilist [1; 2; 3; 4])                  ));
+  run_exn show_int_list  1  q qh (REPR (fun q   -> reverso (ilist [1]) q                           ));
   run_exn show_int_list  1  q qh (REPR (fun q   -> reverso (ilist [1; 2; 3; 4]) q                  ));
   run_exn show_int_list  2  q qh (REPR (fun q   -> reverso q (ilist [1])                           ));
-  run_exn show_int_list  1  q qh (REPR (fun q   -> reverso (ilist [1]) q                           ));
   run_exn show_int       1  q qh (REPR (fun q   -> a_and_b q                                       ));
   run_exn show_int       2  q qh (REPR (fun q   -> a_and_b' q                                      ));
   run_exn show_int      10  q qh (REPR (fun q   -> fives q                                         )); *)
@@ -72,7 +74,15 @@ let _withFree =
   runL          2  q  qh (REPR (fun q   -> reverso q q                                  )); *)
   runL          2 qr qrh (REPR (fun q r -> appendo q (ilist []) r                       ));
   (* runL          1  q  qh (REPR (fun q   -> reverso q q                                  ));
+  run_exn show_int      10  q qh (REPR (fun q   -> fives q                                         )); *)
+  ()
+
+let _withFree () =
+  runL          1  q  qh (REPR (fun q   -> reverso (ilist []) (ilist [])                ));
+  runL          2  q  qh (REPR (fun q   -> reverso q q                                  ));
+  runL          4 qr qrh (REPR (fun q r -> appendo q (ilist []) r                       ));
+  runL          1  q  qh (REPR (fun q   -> reverso q q                                  ));
   runL          2  q  qh (REPR (fun q   -> reverso q q                                  ));
   runL          3  q  qh (REPR (fun q   -> reverso q q                                  ));
-  runL         10  q  qh (REPR (fun q   -> reverso q q                                  )); *)
+  runL         10  q  qh (REPR (fun q   -> reverso q q                                  ));
   ()
--- regression/test123wtf.ml
+++ regression/test123wtf.ml
@@ -3,7 +3,7 @@ open Tester
 open Printf
 open ManualReifiers
 
-let (===) ?loc = unitrace ?loc (fun h x ->
+let (===) ?loc = unitrace (fun h x ->
   GT.show logic string_of_int @@ ManualReifiers.int_reifier h x)
 
 
@@ -77,7 +77,7 @@ let () =
 
 
 
-(*
+
 let rec evalo m =
   fresh (f2)
     (conde
@@ -90,35 +90,8 @@ let rec evalo m =
         ])
     (m === !!5 )
 ;;
-*)
 
-let rec evalo m st =
-  let (f2,idx) = State.new_var st  in
-  printfn "create new variable f2 as _.%d" idx;
-  printfn "create inc   in fresh === (f2)";
-  MKStream.inc (fun () ->
-      printfn "inc in fresh forced === (f2)";
-      printfn " created inc in conde";
-      MKStream.bind
-        (MKStream.inc (fun ()  ->
-              printfn " force a conde";
-              MKStream.mplus
-                 (let (x,idx) = State.new_var st  in
-                  printfn "create new variable x as _.%d" idx;
-                  printfn "create inc   in fresh === (x)";
-                  MKStream.inc (fun ()  ->
-                      printfn "inc in fresh forced === (x)";
-                      (f2 === (!! 1)) st))
-                 (MKStream.inc (fun ()  ->
-                       printfn " force inc from mplus* I";
-                       (let (p2,idx) = State.new_var st  in
-                        printfn "create new variable p2 as _.%d" idx;
-                        printfn "create inc   in fresh === (p2)";
-                        MKStream.inc (fun ()  ->
-                            printfn "inc in fresh forced === (p2)";
-                            (f2 === (!! 3)) st))))))
-        (m === (!! 5))
-  )
+
 
 
 let () =
