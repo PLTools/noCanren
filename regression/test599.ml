f870ec8c2bf8dd492979a91296a3533e3cf2f4d9
kakadu <kakadu.hafanana@gmail.com>
2017-05-12 20:31:48 +0300

    Added show for type t and type t2

--- regression/test599.ml
+++ regression/test599.ml
@@ -1,29 +1,6 @@
-type ('a,'b) t = OK of 'a | Error of 'b;;
+open Test599meta
+open Printf
 
-class type virtual
-      [ 'heck
-      (* , 'a,'a_inh,'a_syn
-      , 'b,'b_inh,'b_syn *)
-      , 'type_itself
-      , 'gt_a_for_a, 'gt_a_for_b
-      ,'inh,'syn]
-      t_meta_tt = object
-  method c_OK : 'inh ->
-                ( 'inh,
-                  'type_itself,
-                  'syn,
-                  'heck) GT.a ->
-                'gt_a_for_a ->
-                'syn
-  method c_Error :  'inh ->
-                    ( 'inh,
-                      'type_itself,
-                      'syn,
-                      'heck) GT.a ->
-                    'gt_a_for_b ->
-                    'syn
-  (* we omitted from meta_tt a method for type itself *)
-end
 class type virtual ['a,'a_inh,'a_syn,'b,'b_inh,'b_syn,'inh,'syn] t_tt
   = object
     inherit [ < a: 'a_inh -> 'a -> 'a_syn
@@ -59,25 +36,17 @@ let (t :
     'syn, unit)
     GT.t)
   =
-  let rec
-    t_gcata
-      transform_a
-      transform_b
-      transformer initial_inh subject =
-    let rec self = t_gcata transform_a transform_b transformer
-    and parameter_transforms_obj =
-      object method a = transform_a method b = transform_b end
-    in
-    match subject with
-    | OK arg0 ->
-        transformer#c_OK initial_inh
-          (GT.make self subject parameter_transforms_obj)
-          (GT.make transform_a arg0 parameter_transforms_obj)
-    | Error arg0 ->
-        transformer#c_Error initial_inh
-          (GT.make self subject parameter_transforms_obj)
-          (GT.make transform_b arg0 parameter_transforms_obj)
-     in
+  let t_gcata transform_a transform_b
+              transformer initial_inh subject =
+    let parameter_transforms_obj =
+      object method a = transform_a method b = transform_b end in
+    t_meta_gcata
+      (fun arg0 -> GT.make transform_a arg0 parameter_transforms_obj)
+      (fun arg0 -> GT.make transform_b arg0 parameter_transforms_obj)
+      parameter_transforms_obj
+      transformer initial_inh subject
+  in
+
   { GT.gcata = t_gcata; plugins = () }
 
 class virtual ['a,'a_inh,'a_syn,'b,'b_inh,'b_syn,'inh,'syn] t_t =
@@ -117,3 +86,32 @@ class virtual ['a,'a_inh,'a_syn,'b,'b_inh,'b_syn,'inh,'syn] t_t =
     method t_t transform_a transform_b =
       GT.transform t transform_a transform_b this
   end
+
+
+let generic_show impl fok ferr () e =
+  match e with
+  | OK s    -> impl#c_OK    () fok s
+  | Error e -> impl#c_Error () ferr e
+
+class ['a, 'b] show_meta_t = fun for_a for_b -> object (this)
+
+end
+class ['a, 'b] show_result = object(this)
+  inherit  ['a,unit,string,'b,unit,string,unit,string] t_t
+  method c_OK    () : _ -> _ -> string
+    = fun _subj p0 -> sprintf "OK %s" (p0.GT.fx ())
+  method c_Error () :
+    _ -> _ -> string
+    = fun _subj p1 -> sprintf "Error %s" (p1.GT.fx ())
+  method qqqq fa fb : unit -> ('a, 'b) t -> string = fun ()  ->
+    GT.transform t fa fb this ()
+  (* method t_t fa fb : unit -> ('a,'b) t -> string = fun () x ->
+    GT.transform t (GT.lift fa) (GT.lift fb) this () x *)
+end
+
+let () =
+  let show fa fb (e: (_,_) t) =
+    t.GT.gcata (GT.lift fa) (GT.lift fb) (new show_result) () e in
+  printf "%s\n%!" (show string_of_int id (OK 1));
+  printf "%s\n%!" (show string_of_int id (Error "asdf"));
+  ()
--- regression/test599_2.ml
+++ regression/test599_2.ml
@@ -1,10 +1,11 @@
-type 'a    t2 = OK of 'a | Error of string;;
+open Test599meta
+open Printf
 
 class type virtual
       [ 'heck
       , 'type_itself
       , 'gt_a_for_a, 'gt_a_for_b
-      ,'inh,'syn]
+      , 'inh,'syn]
       t_meta_tt = object
   method c_OK : 'inh ->
                 ( 'inh,
@@ -52,19 +53,14 @@ let (t2 :
     t2_gcata
       transform_a
       transformer initial_inh subject =
-    let rec self = t2_gcata transform_a transformer
-    and parameter_transforms_obj = object method a = transform_a end
+    let parameter_transforms_obj = object method a = transform_a end
     in
-    match subject with
-    | OK arg0 ->
-        transformer#c_OK initial_inh
-          (GT.make self subject parameter_transforms_obj)
-          (GT.make transform_a arg0 parameter_transforms_obj)
-    | Error arg0 ->
-        transformer#c_Error initial_inh
-          (GT.make self subject parameter_transforms_obj)
-          arg0
-     in
+    t_meta_gcata
+      (fun arg0 -> GT.make transform_a arg0 parameter_transforms_obj)
+      (fun arg0 -> arg0)
+      parameter_transforms_obj
+      transformer initial_inh subject
+  in
   { GT.gcata = t2_gcata; plugins = () }
 
 class virtual ['a,'a_inh,'a_syn,'inh,'syn] t2_t =
@@ -97,3 +93,24 @@ class virtual ['a,'a_inh,'a_syn,'inh,'syn] t2_t =
     method t_t2 transform_a =
       GT.transform t2 transform_a this
   end
+
+class ['a] show_result2 = object(this)
+  inherit  ['a,unit,string,unit,string] t2_t
+  method c_OK    () : _ -> _ -> string
+    = fun _subj p0 -> sprintf "OK %s" (p0.GT.fx ())
+  method c_Error () : _ -> _ -> string
+    = fun _subj p1 -> sprintf "Error %s"
+                        (GT.lift (GT.string.GT.plugins)#show () p1)
+                        (* (p1.GT.fx ()) *)
+  method qqqq fa  : unit -> 'a t2 -> string = fun () ->
+    GT.transform t2 fa this ()
+  (* method t_t fa fb : unit -> ('a,'b) t -> string = fun () x ->
+    GT.transform t (GT.lift fa) (GT.lift fb) this () x *)
+end
+
+let () =
+  let show fa fb (e: _ t2) =
+    t2.GT.gcata (GT.lift fa) (new show_result2) () e in
+  printf "%s\n%!" (show string_of_float id (OK 1.));
+  printf "%s\n%!" (show string_of_int id (Error "asdf"));
+  ()
