e34f4b9e45f3990461b24ec2d4384cef09559d68
kakadu <kakadu.hafanana@gmail.com>
2017-08-07 21:39:24 +0300

    WIP

--- regression/test666.ml
+++ regression/test666.ml
@@ -13,55 +13,61 @@ let just_a a = a === !!5
 let project3 ~msg x y z = project3 ~msg (fun h t -> show_intl_list @@ List.reify ManualReifiers.int h t) x y z
 let project2 ~msg x y   = project2 ~msg (fun h t -> show_intl_list @@ List.reify ManualReifiers.int h t) x y
 
-
-effect AskAppendoCache : (Obj.t * Obj.t * Obj.t) -> Cache3.t
-effect AskReversoCache : (Obj.t * Obj.t * Obj.t) -> Cache3.t
+type ask_result = Hang | Later of Cache3.t;;
+effect AskAppendoCache : (Obj.t * Obj.t * Obj.t) * State.t -> ask_result
+effect AskReversoCache : (Obj.t * Obj.t * Obj.t) * State.t -> ask_result
 
 let rec appendo a b ab = fun st ->
   let _ = ignore @@ project3 ~msg:"Entering appendo" a b ab st in
-  let cache = perform (AskAppendoCache Obj.(repr a, repr b, repr ab)) in
-  let () = printf "Size of cache is %d\n%!" (Cache3.size cache) in
-
-  let inner st0 = conde
-    [ ((a === nil ()) &&& (b === ab))
-    ; Fresh.three @@ fun h t ab' ->
-        ?& [
-          (a === h%t);
-          (h%ab' === ab);
-          (appendo t b ab');
+  match perform (AskAppendoCache (Obj.(repr a, repr b, repr ab), st)) with
+  | Hang -> assert false
+  | Later cache ->
+      let () = printf "Size of cache is %d\n%!" (Cache3.size cache) in
+      let appendo x y xy st =
+        match appendo x y xy st with
+        | x -> x
+        | effect (AskAppendoCache (new_arg,st)) k ->
+            if Cache3.alpha_contains new_arg st cache
+            then continue k Hang
+            else continue k @@ Later Cache3.(extend new_arg cache)
+      in
+      conde
+        [ ((a === nil ()) &&& (b === ab))
+        ; Fresh.three @@ fun h t ab' ->
+            ?& [
+              (a === h%t);
+              (h%ab' === ab);
+              (appendo t b ab');
+            ]
         ]
-    ] st0
-  in
-  match inner st with
-  | x -> x
-  | effect (AskAppendoCache new_arg) k -> continue k Cache3.(extend new_arg cache)
+        st
 
 let rec reverso a b = fun st ->
-  let cache = perform (AskReversoCache Obj.(repr a, repr b, repr 1)) in
+  let cache = perform (AskReversoCache (Obj.(repr a, repr b, repr 1), st)) in
   (project2 ~msg:"reverso" a b &&&
   conde
     [ ((a === nil ()) &&& (b === nil ()))
     ; Fresh.three @@ fun h t a' ->
-	(a === h%t) &&&
+        (a === h%t) &&&
         ?& [
-	     (reverso t a');
-	     (appendo a' !<h b);
-	]
+          (reverso t a');
+          (appendo a' !<h b);
+        ]
     ]) st
 
-let () = 
+let () =
   runL (-1)  q  qh ("", (fun q   -> fun st ->
-				(*let rel st = reverso (ilist [1;2;3]) q st in*)
-				let rel st = appendo (ilist [1;2;3]) (ilist [4;5]) q st in
-				match rel st with
-				| effect (AskAppendoCache arg) k -> 
-                                     printf "handlig askAppendo %s %d\n" __FILE__ __LINE__;
-                                     continue k Cache3.(extend arg empty)
-				| effect (AskReversoCache arg) k -> 
-                                     printf "handlig askReverso\n";
-                                     continue k Cache3.(extend arg empty)
-                                | stream -> stream
-               ));
+        (*let rel st = reverso (ilist [1;2;3]) q st in*)
+        let rel st = appendo (ilist [1;2;3]) (ilist [4;5]) q st in
+        match rel st with
+        | effect (AskAppendoCache (arg,_)) k ->
+              (* printf "handlig askAppendo %s %d\n" __FILE__ __LINE__; *)
+              continue k @@ Later Cache3.(extend arg empty)
+        | effect (AskReversoCache (arg,_)) k ->
+              (* printf "handlig askReverso\n"; *)
+              continue k @@ Later Cache3.(extend arg empty)
+        | stream -> stream
+  ));
   ()
 
 (*
@@ -83,4 +89,3 @@ let _withFree () =
   runL         10  q  qh ("", (fun q   -> reverso q q                                  ));
   ()
 *)
-
