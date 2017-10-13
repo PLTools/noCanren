bc4591b9eccab362900254d7e41b6b7fc3d8c4f9
kakadu <kakadu.hafanana@gmail.com>
2017-10-13 15:27:09 +0300

    First version of fmap generation

--- transl/PutDistrib.ml
+++ transl/PutDistrib.ml
@@ -30,6 +30,53 @@ module FoldInfo = struct
       {param_name; rtyp; ltyp} :: ts
 end
 
+let prepare_fmap tdecl =
+  let open Location in
+  let open Ast_helper in
+  let extract_names = List.map (fun typ ->
+      match typ.ptyp_desc with
+      | Ptyp_var s -> s
+      | _ -> assert false
+    )
+  in
+  let param_names = extract_names (List.map fst tdecl.ptype_params) in
+  let Ptype_variant constructors = tdecl.ptype_kind in
+  let cases = constructors |> List.map (fun cdecl ->
+    let argnames = extract_names cdecl.pcd_args in
+    let cname = cdecl.pcd_name.txt in
+    let clid = mknoloc @@ Longident.Lident cname in
+    let rec make_f_expr name =
+      if name = "self" then apply_self ()
+      else Exp.ident @@ mknoloc @@ Longident.Lident ("f"^name)
+    and apply_self () =
+      match List.filter ((<>)"self") argnames with
+      | [] -> [%expr fmap]
+      | xs -> Exp.(apply [%expr fmap] @@
+                List.map (fun s -> ("", ident @@ mknoloc @@ Longident.Lident ("f"^s))) xs)
+    in
+    let pc_lhs, pc_rhs =
+      let wrap_one_arg s =
+        Exp.(apply (make_f_expr s) ["", ident @@ mknoloc @@ Longident.Lident s] )
+      in
+      match argnames with
+      | [] -> (None, None)
+      | [s] -> Some (Ast_helper.Pat.var (mknoloc s)), (Some (wrap_one_arg s))
+      | ___ -> Some Ast_helper.Pat.(tuple @@ List.map (fun name -> var (mknoloc name)) argnames),
+          Some (Exp.tuple @@ List.map wrap_one_arg argnames)
+    in
+    let pc_lhs = Ast_helper.Pat.construct clid pc_lhs in
+    let pc_rhs = Exp.construct clid pc_rhs in
+    let pc_guard = None in
+    { pc_rhs; pc_lhs; pc_guard }
+  )
+  in
+
+  [%stri let rec fmap = [%e
+    List.fold_right (function
+      | "self" -> fun acc -> acc
+      | name -> Exp.fun_ "" None Pat.(var @@ mknoloc name)
+      ) param_names (Exp.function_ cases) ] ]
+
 let revisit_adt tdecl ctors =
   let tdecl = {tdecl with ptype_attributes =
     List.filter (fun (name,_) -> name.Location.txt <> "put_distrib_here") tdecl.ptype_attributes }
@@ -44,7 +91,7 @@ let revisit_adt tdecl ctors =
             (fun typ (map,args) ->
                   match typ with
                   | [%type: _] -> assert false
-                  | {ptyp_desc = Ptyp_constr ({txt=Longident.Lident name;_},[]); _} when name = der_typ_name ->
+                  | {ptyp_desc = Ptyp_constr ({Asttypes.txt=Longident.Lident name;_},[]); _} when name = der_typ_name ->
                       (FoldInfo.extend "self" typ typ map, [%type: 'self] :: args)
                   | arg -> (map, arg::args)
             )
@@ -68,10 +115,11 @@ let revisit_adt tdecl ctors =
       {full_t with ptype_params = full_t.ptype_params @ extra_params;
                    ptype_name = { full_t.ptype_name with txt = "g" ^ full_t.ptype_name.txt }}
     in
+    let fmap_for_typ = prepare_fmap functor_typ in
     let non_logic_typ =
       let alias_desc =
         let old_params = List.map fst tdecl.ptype_params  in
-        let extra_params = FoldInfo.map ~f:(fun {rtyp} -> rtyp)  mapa in
+        let extra_params = FoldInfo.map ~f:(fun {FoldInfo.rtyp} -> rtyp)  mapa in
         Ptyp_constr (Location.mknoloc (Longident.Lident functor_typ.ptype_name.txt), old_params @ extra_params)
       in
       Pstr_type
@@ -79,7 +127,7 @@ let revisit_adt tdecl ctors =
           ; ptype_manifest = Some { ptyp_loc = Location.none; ptyp_attributes = []; ptyp_desc = alias_desc}
           } ]
     in
-    [ Pstr_type [functor_typ]; non_logic_typ ]
+    [ Pstr_type [functor_typ]; fmap_for_typ.pstr_desc; non_logic_typ ]
 
 let has_to_gen_attr (xs: attributes) =
   try let _ = List.find (fun (name,_) -> name.Location.txt = "put_distrib_here") xs in
