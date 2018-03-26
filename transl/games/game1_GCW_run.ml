12c78a24c393b0cf7c1da39c2d0bd85da56f2b6f
Peter Lozov <not_kill@mail.ru>
2018-03-26 13:03:11 +0300

    Added 3 examples (games) for translator.

---
+++ transl/games/game1_GCW_run.ml
@@ -0,0 +1,24 @@
+open MiniKanren
+open Game1_GCW
+open Tester
+
+
+let show_person = function
+  | G -> "G"
+  | W -> "W"
+  | C -> "C"
+  | N -> "N"
+
+let show_list f x =
+  let rec show_l = function
+  | Nil -> ""
+  | Cons (x, Nil) -> f x
+  | Cons (x, xs) -> Printf.sprintf "%s, %s" (f x) (show_l xs)
+  in
+  Printf.sprintf "[%s]" (show_l x)
+
+let myshow x = show_list show_person x
+
+let () =
+  run_exn myshow (-1) q  qh  ("answers", (fun q -> checkAnswer ((===)q) (!!true)) );
+  ()
