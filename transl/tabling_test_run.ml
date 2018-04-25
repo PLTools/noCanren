6d0ebcdab605a6235a2755ac3183a07824096873
Peter Lozov <not_kill@mail.ru>
2018-04-25 14:49:20 +0300

    Added real fibonacci in tabling test.

--- transl/tabling_test_run.ml
+++ transl/tabling_test_run.ml
@@ -7,7 +7,13 @@ let rec int2num i = if i <= 0 then o () else s (int2num (i-1))
 
 let show_bool x = if x then "true" else "false"
 
+let show_number num =
+  let rec helper = function
+  | O   -> 0
+  | S x -> 1  + (helper x)
+  in
+  string_of_int @@ helper num
+
 let () =
- run_exn show_bool (-1) q qh ("answers", (fun q -> (fib ((===)(int2num 100)) q )));
- run_exn show_bool (-1) q qh ("answers", (fun q -> (fib ((===)(int2num 200)) q )));
+ run_exn show_number (-1) q qh ("answers", (fun q -> (fib ((===)(int2num 23)) q )));
  
