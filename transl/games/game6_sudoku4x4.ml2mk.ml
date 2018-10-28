4b3ea688f6eae3df86cbee8d676e17211ddf1adb
Lozov Peter <not_kill@mail.ru>
2018-10-29 01:10:35 +0300

    Added sudoku 4x4 example.

---
+++ transl/games/game6_sudoku4x4.ml2mk.ml
@@ -0,0 +1,30 @@
+type num = N1 | N2 | N3 | N4
+
+type sudoku4X4 =
+  S4x4 of num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num
+
+let not_eq a b =
+  match a with
+  | N1 -> (match b with N1 -> false | N2 -> true  | N3 -> true  | N4 -> true )
+  | N2 -> (match b with N1 -> true  | N2 -> false | N3 -> true  | N4 -> true )
+  | N3 -> (match b with N1 -> true  | N2 -> true  | N3 -> false | N4 -> true )
+  | N4 -> (match b with N1 -> true  | N2 -> true  | N3 -> true  | N4 -> false)
+
+let all_different a b c d =
+  not_eq a b && not_eq a c && not_eq a d && not_eq b c && not_eq b d && not_eq c d
+
+let check_sudoku sudoku =
+  match sudoku with
+  | S4x4 (a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44) ->
+    all_different a11 a12 a13 a14 &&
+    all_different a21 a22 a23 a24 &&
+    all_different a31 a32 a33 a34 &&
+    all_different a41 a42 a43 a44 &&
+    all_different a11 a21 a31 a41 &&
+    all_different a12 a22 a32 a42 &&
+    all_different a13 a23 a33 a43 &&
+    all_different a14 a24 a34 a44 &&
+    all_different a11 a12 a21 a22 &&
+    all_different a13 a14 a23 a24 &&
+    all_different a31 a32 a41 a42 &&
+    all_different a33 a34 a43 a44
---
+++ transl/games/game6_sudoku4x4_run.ml
@@ -0,0 +1,26 @@
+open MiniKanren
+open MiniKanrenStd
+open Game6_sudoku4x4
+open Tester
+
+let show_num = function
+| N1 -> "1"
+| N2 -> "2"
+| N3 -> "3"
+| N4 -> "4"
+
+let show_sudoku4x4 = function
+| S4x4 (a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44) ->
+  let s = show_num in
+  let s1 = Printf.sprintf "%s %s | %s %s" (s a11) (s a12) (s a13) (s a14) in
+  let s2 = Printf.sprintf "%s %s | %s %s" (s a21) (s a22) (s a23) (s a24) in
+  let s3 = Printf.sprintf "----+----" in
+  let s4 = Printf.sprintf "%s %s | %s %s" (s a31) (s a32) (s a33) (s a34) in
+  let s5 = Printf.sprintf "%s %s | %s %s" (s a41) (s a42) (s a43) (s a44) in
+  Printf.sprintf "%s\n%s\n%s\n%s\n%s" s1 s2 s3 s4 s5
+
+let myshow = show_sudoku4x4
+
+let () =
+  run_exn myshow (1) q qh ("sudoku", (fun q -> (check_sudoku q (!!true))));
+  ()
