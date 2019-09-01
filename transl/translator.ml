fe367942f7c555c233c4da1fc7989c6a9b04a12d
Lozov Peter <not_kill@mail.ru>
2019-09-01 19:47:01 +0300

    Removed commented code.

--- transl/translator.ml
+++ transl/translator.ml
@@ -698,22 +698,7 @@ let rec create_fresh_argument_names_by_type (typ : Types.type_expr) =
     else fail_loc loc "Pattern matching contains unified patterns"
 
   and translate_bool_funs is_or =
-    if is_or then  [%expr Bool.oro] else  [%expr Bool.ando]
-    (* let a1  = create_fresh_var_name () in
-    let a2  = create_fresh_var_name () in
-    let q   = create_fresh_var_name () in
-    let fst = if is_or then [%expr !!true]  else [%expr !!false] in
-    let snd = if is_or then [%expr !!false] else [%expr !!true]  in
-    if need_false then
-    [%expr fun [%p create_pat a1] [%p create_pat a2] [%p create_pat q] ->
-             conde [([%e create_id a1] === [%e fst]) &&& ([%e create_id q] === [%e fst]);
-                    ([%e create_id a1] === [%e snd]) &&& ([%e create_id q] === [%e create_id a2])]]
-    else if is_or then
-    [%expr fun [%p create_pat a1] [%p create_pat a2] [%p create_pat q] ->
-             ([%e create_id q] === !!true) &&& (([%e create_id a1] === !!true) ||| ([%e create_id a2] === !!true))]
-    else
-    [%expr fun [%p create_pat a1] [%p create_pat a2] [%p create_pat q] ->
-             ([%e create_id q] === !!true) &&& (([%e create_id a1] === !!true) &&& ([%e create_id a2] === !!true))] *)
+    if is_or then [%expr Bool.oro] else [%expr Bool.ando]
 
   and translate_eq_funs is_eq =
     let a1  = create_fresh_var_name () in
@@ -730,11 +715,6 @@ let rec create_fresh_argument_names_by_type (typ : Types.type_expr) =
              ([%e create_id a1] === [%e create_id a2]) &&& ([%e create_id q] === [%e fst])]
 
   and translate_not_fun () = [%expr Bool.noto]
-    (* let a  = create_fresh_var_name () in
-    let q  = create_fresh_var_name () in
-    [%expr fun [%p create_pat a] [%p create_pat q] ->
-             conde [([%e create_id a] === !!true ) &&& ([%e create_id q] === !!false);
-                    ([%e create_id a] === !!false) &&& ([%e create_id q] === !!true )]] *)
 
   and translate_if let_vars cond th el typ =
     let args = create_fresh_argument_names_by_type typ in
