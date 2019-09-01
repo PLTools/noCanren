087faf84f345ca0c4494c977eb90727e11af8919
Lozov Peter <not_kill@mail.ru>
2019-09-02 00:54:46 +0300

    Updated lowercase for some special ctors.

--- transl/util.ml
+++ transl/util.ml
@@ -1,5 +1,6 @@
 let mangle_construct_name name =
   let low = String.mapi (function 0 -> Char.lowercase | _ -> fun x -> x ) name in
   match low with
-  | "val" | "if" | "else" | "for" | "do" | "let" | "open" | "not" | "pair" | "conj" -> low ^ "_"
+  | "val" | "if" | "else" | "for" | "do" | "let" | "open" | "not" | "pair" | "conj"
+          | "var" | "snd" | "fst" -> low ^ "_"
   | _ -> low
