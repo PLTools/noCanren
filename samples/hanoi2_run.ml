b01fa999c598620c5f47b1dfe839a52bf775277c
Peter Lozov <not_kill@gmail.ru>
2023-02-12 22:44:28 +0300

    Updated emaxple 'hanoi'.

--- samples/hanoi2_run.ml
+++ samples/hanoi2_run.ml
@@ -2,21 +2,15 @@ module L = Stdlib.List
 open GT
 open OCanren
 open OCanren.Std
+open Hanoi2
 open Hanoi2.HO
 
 (*************************************************)
 
-let show_pin = function
-  | A -> "A"
-  | B -> "B"
-  | C -> "C"
-;;
-
 let show_answer = show List.ground @@ show Pair.ground show_pin show_pin
-let rec toN n = if n = 0 then !!Z else !!(S (toN (n - 1)))
 
 let gen_pin n =
-  let rec gen_pin m = if m = n then nil () else toN m % gen_pin (m + 1) in
+  let rec gen_pin m = if m = n then nil () else nat m % gen_pin (m + 1) in
   gen_pin 0
 ;;
 
@@ -30,6 +24,6 @@ let _ =
   @@ Stream.take ~n:1
   @@ run
        q
-       (fun q -> eval (( === ) q) (( === ) (start 3)) (finish 3))
+       (fun q -> FO.eval q (start 3) (finish 3))
        (fun rr -> rr#reify (Std.List.prj_exn (Std.Pair.prj_exn prj_exn prj_exn)))
 ;;
